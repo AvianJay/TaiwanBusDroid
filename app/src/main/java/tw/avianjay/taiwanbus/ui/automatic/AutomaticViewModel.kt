6aeb34121c46043882bff5e68acd2d48ddaceb9c
AvianJay <avianjayusb@gmail.com>
2024-12-06 23:08:54 +0800

    change example code name

--- app/src/main/java/tw/avianjay/taiwanbus/ui/automatic/AutomaticViewModel.kt
+++ app/src/main/java/tw/avianjay/taiwanbus/ui/automatic/AutomaticViewModel.kt
@@ -1,13 +1,13 @@
-package tw.avianjay.taiwanbus.ui.notifications
+package tw.avianjay.taiwanbus.ui.automatic
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class AutomaticViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/tw/avianjay/taiwanbus/ui/settings/SettingsFragment.kt
+++ app/src/main/java/tw/avianjay/taiwanbus/ui/settings/SettingsFragment.kt
@@ -1,4 +1,4 @@
-package tw.avianjay.taiwanbus.ui.notifications
+package tw.avianjay.taiwanbus.ui.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import tw.avianjay.taiwanbus.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -23,7 +23,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
