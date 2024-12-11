1fd377861929cf213068f04d0f27db5fb8415f7f
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-12-11 05:09:50 +0000

    update python script

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,12 +1,18 @@
 # My code is shit.
 import taiwanbus
 import asyncio
-route = ""
+route_name = ""
 paths_name = []
 paths = []
 
 def get_bus(routekey):
-    route = asyncio.run(taiwanbus.get_complete_bus_info(routekey))
+    global route_name, paths_name, paths
+    paths = asyncio.run(taiwanbus.get_complete_bus_info(routekey))
+    path = asyncio.run(taiwanbus.fetch_paths(routekey))
+    for p in path:
+        paths_name.append(p["path_name"])
+    route = asyncio.run(taiwanbus.fetch_route(routekey))
+    route_name = route[0]["route_name"]
 
 def get_formated_info(route_key):
     info = asyncio.run(taiwanbus.get_complete_bus_info(route_key))
